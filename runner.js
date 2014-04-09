f21ce74e5226dfaf20120504b166795407f9c574
Justin Wehrman <Justin.Wehrman@RedFuse.com>
2014-04-09 10:44:53 -0500

    removed email entries

--- runner.js
+++ runner.js
@@ -5,12 +5,11 @@ var rule = new schedule.RecurrenceRule();
 rule.minute = 59;
 
 var j = schedule.scheduleJob(rule, function() {
-    console.log('It\'s the Final Countdown');
     sendmail({
-        from: 'Justin.Wehrman@RedFuse.com',
-        to: 'Matt.Brown@RedFuse.com',
-        subject: 'It\'s the Final Countdown',
-        content: 'https://www.youtube.com/watch?v=9jK-NcRmVcw',
+        from: '',
+        to: '',
+        subject: '',
+        content: '',
       }, function(err, reply) {
         console.log(err && err.stack);
         console.dir(reply);
